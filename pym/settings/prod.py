9bb16419569ae52cf72e1354cd645ff343be9ce9
unzaintomas <156114109+unzaintomas@users.noreply.github.com>
2024-01-09 17:39:16 -0300

    Update prod.py

--- pym/settings/prod.py
+++ pym/settings/prod.py
@@ -23,5 +23,5 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = [BASE_DIR / 'staticfiles']
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+STATIC_ROOT = BASE_DIR.child('staticfiles')
